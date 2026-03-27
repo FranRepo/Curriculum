c6ee194c1d6747a984bfaa4771c8934a10569f9e
FranRepo <francisca.prado.6lsf@gmail.com>
2026-03-27 00:37:51 -0300

    commit

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ function scrollToSection(id) {
     behavior: "smooth"
   });
 }
+
+window.addEventListener("load", () => {
+    const fills = document.querySelectorAll(".fill");
+  
+    fills.forEach(fill => {
+      const width = fill.style.width;
+      fill.style.width = "0";
+  
+      setTimeout(() => {
+        fill.style.transition = "width 1.5s ease";
+        fill.style.width = width;
+      }, 300);
+    });
+});
