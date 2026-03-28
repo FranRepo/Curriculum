2a0f39df2209a5825c168b949bf04a8708b2ae23
FranRepo <francisca.prado.6lsf@gmail.com>
2026-03-28 02:06:01 -0300

    commit

--- script.js
+++ script.js
@@ -4,24 +4,9 @@ function scrollToSection(id) {
   });
 }
 
-const fills = document.querySelectorAll(".fill");
+const donuts = document.querySelectorAll(".donut");
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      const fill = entry.target;
-      const width = fill.getAttribute("data-width");
-
-      fill.style.width = width;
-    }
-  });
-}, { threshold: 0.5 });
-
-fills.forEach(fill => {
-  const width = fill.style.width;
-
-  fill.style.width = "0";
-  fill.setAttribute("data-width", width);
-
-  observer.observe(fill);
+donuts.forEach(donut => {
+  const percent = donut.getAttribute("data-percent");
+  donut.style.setProperty("--percent", percent);
 });
